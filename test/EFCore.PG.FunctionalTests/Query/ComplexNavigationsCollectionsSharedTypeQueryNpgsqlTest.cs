ca43015dacd3cf9cd4071dd82f96bff5e7cf3fcd
Giang Pham <giang.phamhoang712@gmail.com>
2024-02-01 13:57:09 +1100

    Fix some more tests

--- test/EFCore.PG.FunctionalTests/Query/ComplexNavigationsCollectionsSharedTypeQueryNpgsqlTest.cs
+++ test/EFCore.PG.FunctionalTests/Query/ComplexNavigationsCollectionsSharedTypeQueryNpgsqlTest.cs
@@ -1,3 +1,5 @@
+using Npgsql.EntityFrameworkCore.PostgreSQL.TestUtilities;
+
 namespace Npgsql.EntityFrameworkCore.PostgreSQL.Query;
 
 public class ComplexNavigationsCollectionsSharedTypeQueryNpgsqlTest : ComplexNavigationsCollectionsSharedTypeQueryRelationalTestBase<
@@ -10,5 +12,11 @@ public class ComplexNavigationsCollectionsSharedTypeQueryNpgsqlTest : ComplexNav
     {
         Fixture.TestSqlLoggerFactory.Clear();
         Fixture.TestSqlLoggerFactory.SetTestOutputHelper(testOutputHelper);
+    }
+
+    [SkipForCockroachDb("Incompatible")]
+    public override Task Complex_query_with_let_collection_projection_FirstOrDefault_with_ToList_on_inner_and_outer(bool async)
+    {
+        return base.Complex_query_with_let_collection_projection_FirstOrDefault_with_ToList_on_inner_and_outer(async);
     }
 }
